bb02a171d1d861b634285be75f7d266c6244ab8d
Glitch (getting-started-discord) <none>
2022-10-23 13:11:20 +0000

    👯🏝 Checkpoint ./app.js:42180807/818 ./commands.js:42180807/1847

--- app.js
+++ app.js
@@ -2,40 +2,37 @@ import express from 'express'
 import axios from 'axios';
 import { InteractionType, InteractionResponseType } from 'discord-interactions';
 import { VerifyDiscordRequest, getRandomEmoji, ComponentType, ButtonStyle, DiscordAPI } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
-import { CHALLENGE_COMMAND, TEST_COMMAND, HasGuildCommands } from './commands.js';
+
+import { CHALLENGE_COMMAND, TEST_COMMAND, HasGuildCommands, InstallGuildCommand } from './commands.js';
 
 const app = express();
 app.use(express.json({verify: VerifyDiscordRequest(process.env.PUBLIC_KEY)}));
+
 const client = axios.create({
     headers: {'Authorization': `Bot ${process.env.DISCORD_TOKEN}`}
 });
 
-let activeGames = {};
-
-app.post('/test', function (req, res) {
-    let { type, id, data } = req.body;
+InstallGuildCommand(client, "1031396629070225438", "988515701318901770", TEST_COMMAND)
 
 
-    if (type === InteractionType.PING) {
-        return res.json({ "type": InteractionResponseType.PONG });
-    };
+app.post('/interactions', function (req, res) {
+    const { type, id, data } = req.body;
+    const { name } = data;
 
-    if (type === InteractionType.APPLICATION_COMMAND){
-        let { name } = data;
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      
         if (name === "test") {
-            return res.send({
-                "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            return res.send({ "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 "data": {
-                    // Fetches a random emoji to send from a helper function
                     "content": "hello world " + getRandomEmoji()
                 }
             });
         }
-
+      
     }
+  
 });
 
-app.listen(3000, () => {
+app.listen(3000, async() => {
     console.log('Listening on port 3000');
 });
